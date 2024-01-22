96c3165f367fe394103529ef917ad14ee7963c3e
WalterLuong <wluong@student.42.fr>
2024-01-22 10:15:38 +0100

    feat(frontend): layout of the Home page, with a navbar layout

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="bg-gray-300 flex absolute bottom-0 w-screen overflow-hidden justify-around h-12 items-center border-t border-black">
+      <p>Test</p>
+      <p>Test</p>
+      <p>Test</p>
+      <p>Test</p>
+    </div>
+  );
+};
+
+export default Navbar;
