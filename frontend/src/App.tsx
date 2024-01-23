57d576f56366b5128d43e133c64dbb9d1a624022
WalterLuong <wluong@student.42.fr>
2024-01-23 11:12:45 +0100

    fix(frontend): add new icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="bg-slate-200 h-screen w-screen">
+      <div className="bg-slate-200 h-screen w-screen overflow-hidden">
         <img src="/lvmh-logo.png" alt="LVMH_logo" className="h-32 m-auto" />
         <h1 className="font-bold text-2xl">Je ne sais pas ce que je fais</h1>
       </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import { AccountCircle, Favorite, Home, Settings } from "@material-ui/icons";
+
 const Navbar = () => {
   return (
-    <div className="bg-gray-300 flex absolute bottom-0 w-screen overflow-hidden justify-around h-12 items-center border-t border-black">
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
+    <div className="bg-slate-200 flex absolute bottom-0 w-screen overflow-hidden justify-around h-12 items-center border-t border-black">
+      <Home className="font-bold" />
+      <AccountCircle className="font-bold" />
+      <Favorite className="font-bold" />
+      <Settings className="font-bold" />
     </div>
   );
 };
